b4d2de8a173e4d15265fa4eb673acb4957d0b5b0
Janassalem <janamohamed248@gmail.com>
2024-06-26 02:25:36 +0300

    Update daily_reminderpattern_drawing.py to print reminder message in the required format

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -4,10 +4,12 @@ def main():
     try:
         size = int(input("Enter the size of the pattern: "))
         if size > 0:
-            for i in range(size):
+            i = 0
+            while i < size:
                 for j in range(size):
                     print("*", end="")
                 print()  # Move to the next line after printing a row
+                i += 1
         else:
             print("Please enter a positive integer.")
     except ValueError:
