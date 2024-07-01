86375c692b6a082199db00fc42237b7a0cf0f703
Janassalem <janamohamed248@gmail.com>
2024-07-01 22:51:07 +0300

    Initial commit: Add scripts for arithmetic operations, shopping list manager, datetime exploration, and temperature conversion tool

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,12 +8,13 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "Cannot divide by zero"
+        else:
+            return num1 / num2
     else:
         return "Invalid operation"
+
     
     # main.py
 
