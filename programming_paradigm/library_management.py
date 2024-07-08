6e576441baf447cbd26f93059a5a88c36daf2c99
Janassalem <janamohamed248@gmail.com>
2024-07-09 00:47:17 +0300

    .

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -43,25 +43,3 @@ class Library:
         for book in self._books:
             if book.is_available():
                 print(f"{book.title} by {book.author}")
-
-
-if __name__ == "__main__":
-    from library_management import Book, Library
-
-    def main():
-        library = Library()
-        library.add_book(Book("Brave New World", "Aldous Huxley"))
-        library.add_book(Book("1984", "George Orwell"))
-
-        print("Available books after setup:")
-        library.list_available_books()
-
-        library.check_out_book("1984")
-        print("\nAvailable books after checking out '1984':")
-        library.list_available_books()
-
-        library.return_book("1984")
-        print("\nAvailable books after returning '1984':")
-        library.list_available_books()
-
-    main()
